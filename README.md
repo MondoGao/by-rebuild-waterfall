2a2f90ec5be70c4dec6d927a05590463669dc35a
风之雪 <tiancaigaohua@163.com>
2016-11-28 22:13:53 +0800

    + add vue-cli's simple webpack category structure * change dev-server to browser-sync * update readme.md

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 #### Added
 - 初始化仓库
 - 位于 mondo/task1 下
+
+## 学习记录
+### 完成
+- 配置并使用 eslint 其 YAML 格式配置文件
+    - .eslintignore 中相对路径不能加 ./
+- 使用 vue-cli 进行简单的 vue 项目结构构建，并将其默认的 webpack-dev-server + webpack 改为 gulp + browser-sync + webpack-stream + webpack
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,29 @@
-var gulp = require('gulp');
-var server = require('browser-sync').create();
+let gulp = require('gulp');
+
+let webpackStream = require('webpack-stream');
+let webpack = require('webpack');
+let server = require('browser-sync').create();
+let del = require('del');
 
 
-gulp.task('server', function (cb) {
-    server.init({
-        ui: {
-            port: 80
-        },
-        server: {
-            baseDir: './',
-            directory: true
-        },
-        open: 'ui',
-        startPath: '/dist'
-    }, cb);
+gulp.task('webpack', function () {
+  return gulp.src('./src/main.js')
+    .pipe(webpackStream(require('./webpack.config.js'), webpack))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('server', ['webpack'], function (cb) {
+  server.init({
+    ui: {
+      port: 80
+    },
+    server: {
+      baseDir: './',
+      directory: true
+    },
+    open: false,
+    startPath: '/dist'
+  }, cb);
 });
 
 gulp.task('default', ['server']);
